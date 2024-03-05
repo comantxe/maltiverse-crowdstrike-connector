4b2f64bcfb4edfe16efd83c4cf6a640fe7417850
comantxe <gomezmartinantonio@gmail.com>
2024-03-05 09:59:47 +0100

    Requirements + tag feed id

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # maltiverse-crowdstrike-connector
 A connector to upload Maltiverse Threat Intelligence to Crowdstrike
+```
+usage: maltiverse-crowdstrike-connector.py [-h] --maltiverse_api_key MALTIVERSE_API_KEY --crowdstrike_client_id CROWDSTRIKE_CLIENT_ID --crowdstrike_client_secret
+                                           CROWDSTRIKE_CLIENT_SECRET [--crowdstrike_base_url CROWDSTRIKE_BASE_URL] --feed_id FEED_ID [--action ACTION]
 
 options:
-
-  -h, --help                                                show this help message and exit
-  
-  --maltiverse_api_key MALTIVERSE_API_KEY                   Specifies Maltiverse API KEY. Required
-  
-  --crowdstrike_client_id CROWDSTRIKE_CLIENT_ID             Specify the Crowdstrike CLIENT_ID. Required
-  
-  --crowdstrike_client_secret CROWDSTRIKE_CLIENT_SECRET     Specifies Crowdstrike CLIENT_SECRET. Required
-  
-  --feed_id FEED_ID                                         Specifies Maltiverse Feed ID to upload to CrowdStrike Falcon cloud. Required
-  
-  --action ACTION                                           Specifies the action that applies to the uploading indicators. Default: detect
+  -h, --help            show this help message and exit
+  --maltiverse_api_key MALTIVERSE_API_KEY
+                        Specifies Maltiverse API KEY. Required
+  --crowdstrike_client_id CROWDSTRIKE_CLIENT_ID
+                        Specify the Crowdstrike CLIENT_ID.
+  --crowdstrike_client_secret CROWDSTRIKE_CLIENT_SECRET
+                        Specifies Crowdstrike CLIENT_SECRET.
+  --crowdstrike_base_url CROWDSTRIKE_BASE_URL
+                        Specifies Crowdstrike Base URL.
+  --feed_id FEED_ID     Specifies Maltiverse Feed ID to upload to CrowdStrike Falcon cloud. Required
+  --action ACTION       Specifies the action that applies to the uploading indicators. Default: detect
+```
--- maltiverse-crowdstrike-connector.py
+++ maltiverse-crowdstrike-connector.py
@@ -16,12 +16,13 @@ from falconpy import APIHarnessV2
 
 
 class MaltiverseCrowdStrikeHandler:
-    def __init__(self, api_key, client_id, client_secret):
+    def __init__(self, api_key, client_id, client_secret, base_url=None):
         self.api_key = api_key
         self.base_url = "https://api.maltiverse.com"
         self.headers = {"Authorization": f"Bearer {self.api_key}"}
 
         self.falcon = APIHarnessV2(
+            base_url=base_url,
             client_id=client_id,
             client_secret=client_secret,
         )
@@ -42,7 +43,7 @@ class MaltiverseCrowdStrikeHandler:
         response = requests.get(url, headers=self.headers)
         return response.json()
 
-    def convert_maltiverse_feed_to_crowdstrike(self, feed_id):
+    def convert_maltiverse_feed_to_crowdstrike(self, feed_id, action="detect"):
         """
         Converts a maltiverse feed in its original format to a Crowdstrike format
         """
@@ -51,8 +52,10 @@ class MaltiverseCrowdStrikeHandler:
 
         cs_indicators = []
         for element in raw_maltiverse_feed:
-            objs = handler.convert_obj_maltiverse_to_crowdstrike(element)
-            for obj in objs:
+            objs = handler.convert_obj_maltiverse_to_crowdstrike(
+                element, action=action, tag=[feed_id]
+            )
+            for obj in objs or []:
                 cs_indicators.append(obj)
 
         response = {
@@ -61,19 +64,45 @@ class MaltiverseCrowdStrikeHandler:
         }
         return response
 
-    def upload_maltiverse_feed_to_crowdstrike(self, feed_id):
+    def upload_maltiverse_feed_to_crowdstrike(
+        self, feed_id, action="detect", max_chunk_size=100
+    ):
         """
         Given its FEED_ID Uploads a Maltiverse feed to Crowdstrike
         """
-        return self.falcon.command(
-            "indicator_create_v1",
-            retrodetects=False,
-            ignore_warnings=False,
-            body=self.convert_maltiverse_feed_to_crowdstrike(feed_id),
-        )
+
+        ret_array = []
+
+        full_feed = self.convert_maltiverse_feed_to_crowdstrike(feed_id, action=action)
+
+        number_of_ioc = len(full_feed["indicators"])
+        number_of_chunks = int(number_of_ioc / max_chunk_size)
+
+        for count in range(0, number_of_chunks):
+            first_element = count * max_chunk_size
+            last_element = (count + 1) * max_chunk_size
+            if count == number_of_chunks:
+                # case last chunk
+                last_element = -1
+
+            chunk = {
+                "comment": full_feed["comment"],
+                "indicators": full_feed["indicators"][first_element:last_element],
+            }
+            print(json.dumps(chunk, indent=4))
+            print()
+            ret = self.falcon.command(
+                "indicator_create_v1",
+                retrodetects=False,
+                ignore_warnings=False,
+                body=chunk,
+            )
+            print(ret)
+            ret_array.append(ret)
+        return ret_array
 
     def convert_obj_maltiverse_to_crowdstrike(
-        self, maltiverse_ioc, action="detect", expiration_days=1
+        self, maltiverse_ioc, action="detect", expiration_days=1, tag=[]
     ):
         """
         Given a Maltiverse IoC returns a CrowdStrike IoC
@@ -81,7 +110,7 @@ class MaltiverseCrowdStrikeHandler:
         ret = []
         expiration_time = None
         if expiration_days:
-            expiration_datetime = datetime.now() + timedelta(days=expiration_days)
+            expiration_datetime = datetime.utcnow() + timedelta(days=expiration_days)
             expiration_time = expiration_datetime.isoformat() + "Z"
 
         crowdstrike_ioc_type = None
@@ -93,8 +122,8 @@ class MaltiverseCrowdStrikeHandler:
             crowdstrike_ioc_type = "domain"
             crowdstrike_ioc_value = maltiverse_ioc["hostname"]
         if maltiverse_ioc["type"] == "url":
-            crowdstrike_ioc_type = "url"
-            crowdstrike_ioc_value = maltiverse_ioc["url"]
+            # IoC type not supported
+            return None
         if maltiverse_ioc["type"] == "sample":
             crowdstrike_ioc_type = "sha256"
             crowdstrike_ioc_value = maltiverse_ioc["sha256"]
@@ -105,27 +134,26 @@ class MaltiverseCrowdStrikeHandler:
         if maltiverse_ioc["classification"] == "suspicious":
             crowdstrike_ioc_severity = "MEDIUM"
 
-        for bl in maltiverse_ioc["blacklist"]:
-            crowdstrike_ioc = {
-                "action": action,
-                "applied_globally": True,
-                "expiration": expiration_time,
-                "severity": crowdstrike_ioc_severity,
-                "source": bl["source"],
-                "platforms": ["Mac", "Windows", "Linux"],
-                "description": bl["description"],
-                "type": crowdstrike_ioc_type,
-                "value": crowdstrike_ioc_value,
-            }
+        crowdstrike_ioc = {
+            "action": action,
+            "applied_globally": True,
+            "expiration": expiration_time,
+            "severity": crowdstrike_ioc_severity,
+            "source": "Maltiverse",
+            "platforms": ["Mac", "Windows", "Linux"],
+            "description": ", ".join(
+                bl["description"] for bl in maltiverse_ioc["blacklist"]
+            ),
+            "type": crowdstrike_ioc_type,
+            "value": crowdstrike_ioc_value,
+        }
 
-            if "tag" in maltiverse_ioc:
-                crowdstrike_ioc["tags"] = maltiverse_ioc["tag"]
+        if "tag" in maltiverse_ioc:
+            crowdstrike_ioc["tags"] = maltiverse_ioc["tag"] + tag
 
-            if "filename" in maltiverse_ioc:
-                crowdstrike_ioc["metadata"] = {
-                    "filename": maltiverse_ioc["filename"][0]
-                }
-            ret.append(crowdstrike_ioc)
+        if "filename" in maltiverse_ioc:
+            crowdstrike_ioc["metadata"] = {"filename": maltiverse_ioc["filename"][0]}
+        ret.append(crowdstrike_ioc)
         return ret
 
 
@@ -151,6 +179,13 @@ if __name__ == "__main__":
         help="Specifies Crowdstrike CLIENT_SECRET.",
     )
     parser.add_argument(
+        "--crowdstrike_base_url",
+        dest="crowdstrike_base_url",
+        required=False,
+        help="Specifies Crowdstrike Base URL.",
+    )
+
+    parser.add_argument(
         "--feed_id",
         dest="feed_id",
         required=True,
@@ -168,6 +203,7 @@ if __name__ == "__main__":
         arguments.maltiverse_api_key,
         arguments.crowdstrike_client_id,
         arguments.crowdstrike_client_secret,
+        base_url=arguments.crowdstrike_base_url,
     )
 
     res = handler.upload_maltiverse_feed_to_crowdstrike(
